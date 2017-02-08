0c356ac70d5fd87b8f0ff06b34bcca9568c0caae
Italo Romeiro Wanderley <italo.wanderley@arctouch.com>
2017-02-08 16:16:36 -0200

    SplashScreen was done
    The SplashScreen was done by using a theme as an approach. This approach basically
    does the SplashScreen waits for the next screen to be entirely prepared before
    finish itself.
    
    The best point in this approach is that we don't need to be concerned about
    the time the OS will need to do its tasks. The OS always will wait the second
    view signalize it is prepared to be launched to end the SplashScreen.

---
+++ app/src/main/java/com/arctouch/wanderley/italo/movieforyou/ui/activities/SplashScreenActivity.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2017 ArcTouch LLC.
+ * All rights reserved.
+ *
+ * This file, its contents, concepts, methods, behavior, and operation
+ * (collectively the "Software") are protected by trade secret, patent,
+ * and copyright laws. The use of the Software is governed by a license
+ * agreement. Disclosure of the Software to third parties, in any form,
+ * in whole or in part, is expressly prohibited except as authorized by
+ * the license agreement.
+ */
+
+package com.arctouch.wanderley.italo.movieforyou.ui.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by italowanderley on 08/02/17.
+ */
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
